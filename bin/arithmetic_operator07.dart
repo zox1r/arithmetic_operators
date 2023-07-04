1cac5db4708503e2dd382760b09c9d2e749dbde4
zox1r <qarshiyevzoxir@gmail.com>
2023-07-04 11:10:14 +0200

    bin/arithmetic_operator07.dart

--- bin/arithmetic_operator07.dart
+++ bin/arithmetic_operator07.dart
@@ -9,4 +9,10 @@ Find the expression value assign to 'answer'.  a - b + c
 */
 void main() {
   //write your code here
+  int a = 12;
+  int b = 4; 
+  int c = 1;
+  int answer = a - b + c;
+  print(answer);
+
 }
